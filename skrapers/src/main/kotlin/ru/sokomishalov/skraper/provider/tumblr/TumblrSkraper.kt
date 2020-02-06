c7c8d3522e70c935717d0403e7a4ee6ce8119cfe
sokomishalov <sokomishalov@mail.ru>
2020-02-07 01:57:40 +0300

    tumblr further progress

--- skrapers/src/main/kotlin/ru/sokomishalov/skraper/provider/tumblr/TumblrSkraper.kt
+++ skrapers/src/main/kotlin/ru/sokomishalov/skraper/provider/tumblr/TumblrSkraper.kt
@@ -1,15 +1,44 @@
 package ru.sokomishalov.skraper.provider.tumblr
 
+import org.jsoup.nodes.Document
 import ru.sokomishalov.skraper.Skraper
 import ru.sokomishalov.skraper.SkraperClient
 import ru.sokomishalov.skraper.client.jdk.DefaultBlockingSkraperClient
+import ru.sokomishalov.skraper.fetchDocument
+import ru.sokomishalov.skraper.internal.jsoup.getSingleElementByClassOrNull
+import ru.sokomishalov.skraper.internal.jsoup.getSingleElementByTagOrNull
 import ru.sokomishalov.skraper.model.ImageSize
 import ru.sokomishalov.skraper.model.Post
 
 class TumblrSkraper(
         override val client: SkraperClient = DefaultBlockingSkraperClient
 ) : Skraper {
+
     override val baseUrl: String = "https://tumblr.com"
-    override suspend fun getPageLogoUrl(uri: String, imageSize: ImageSize): String? = getProviderLogoUrl()
-    override suspend fun getLatestPosts(uri: String, limit: Int): List<Post> = emptyList()
+
+    override suspend fun getPageLogoUrl(uri: String, imageSize: ImageSize): String? {
+        val document = getPage(uri)
+
+        return document
+                ?.getSingleElementByClassOrNull("user-avatar")
+                ?.getSingleElementByTagOrNull("img")
+                ?.attr("src")
+    }
+
+    override suspend fun getLatestPosts(uri: String, limit: Int): List<Post> {
+        val document = getPage(uri)
+
+        val articles = document
+                ?.getElementsByTag("article")
+                ?.take(limit)
+                .orEmpty()
+
+        // todo
+
+        return emptyList()
+    }
+
+    private suspend fun getPage(uri: String): Document? {
+        return client.fetchDocument("https://${uri}.tumblr.com")
+    }
 }
