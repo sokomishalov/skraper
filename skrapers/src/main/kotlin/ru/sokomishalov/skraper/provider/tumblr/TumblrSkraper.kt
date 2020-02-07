1495db0800007adb233e2aad0e01f9b4597babb6
sokomishalov <sokolovm@dev.vtb>
2020-02-07 13:30:41 +0300

    further progress

--- skrapers/src/main/kotlin/ru/sokomishalov/skraper/provider/tumblr/TumblrSkraper.kt
+++ skrapers/src/main/kotlin/ru/sokomishalov/skraper/provider/tumblr/TumblrSkraper.kt
@@ -5,8 +5,11 @@ import ru.sokomishalov.skraper.Skraper
 import ru.sokomishalov.skraper.SkraperClient
 import ru.sokomishalov.skraper.client.jdk.DefaultBlockingSkraperClient
 import ru.sokomishalov.skraper.fetchDocument
+import ru.sokomishalov.skraper.internal.consts.DEFAULT_POSTS_ASPECT_RATIO
 import ru.sokomishalov.skraper.internal.jsoup.getSingleElementByClassOrNull
 import ru.sokomishalov.skraper.internal.jsoup.getSingleElementByTagOrNull
+import ru.sokomishalov.skraper.model.Attachment
+import ru.sokomishalov.skraper.model.AttachmentType.IMAGE
 import ru.sokomishalov.skraper.model.ImageSize
 import ru.sokomishalov.skraper.model.Post
 
@@ -33,9 +36,27 @@ class TumblrSkraper(
                 ?.take(limit)
                 .orEmpty()
 
-        // todo
-
-        return emptyList()
+        return articles.map { a ->
+            Post(
+                    id = a.attr("data-post-id"),
+                    caption = "",
+                    attachments = a.getElementsByTag("figure").map { f ->
+                        val img = f.getSingleElementByTagOrNull("img")
+                        Attachment(
+                                type = IMAGE,
+                                url = img?.attr("src").orEmpty(),
+                                aspectRatio = img.let {
+                                    val width = it?.attr("data-orig-width")?.toDoubleOrNull()
+                                    val height = it?.attr("data-orig-height")?.toDoubleOrNull()
+                                    when {
+                                        width != null && height != null -> width / height
+                                        else -> DEFAULT_POSTS_ASPECT_RATIO
+                                    }
+                                }
+                        )
+                    }
+            )
+        }
     }
 
     private suspend fun getPage(uri: String): Document? {
