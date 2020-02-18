76624a6ef872049bfa41e9eccfc476fc2c4756c2
sokomishalov <sokolovm@dev.vtb>
2020-02-18 14:04:23 +0300

    further progress

--- example/android/app/src/main/java/ru/sokomishalov/skraper/example/activity/ListViewActivity.kt
+++ example/android/app/src/main/java/ru/sokomishalov/skraper/example/activity/ListViewActivity.kt
@@ -1,19 +1,23 @@
 package ru.sokomishalov.skraper.example.activity
 
 import android.os.Bundle
+import android.view.View
 import android.widget.ListView
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.*
 import ru.sokomishalov.skraper.SkraperClient
 import ru.sokomishalov.skraper.client.okhttp3.OkHttp3SkraperClient
 import ru.sokomishalov.skraper.example.R
-import ru.sokomishalov.skraper.example.adapter.ListViewModelAdapter
+import ru.sokomishalov.skraper.example.adapter.PostsAdapter
 import ru.sokomishalov.skraper.provider.facebook.FacebookSkraper
 import ru.sokomishalov.skraper.provider.facebook.getUserPosts
 import ru.sokomishalov.skraper.provider.instagram.InstagramSkraper
 import ru.sokomishalov.skraper.provider.instagram.getUserPosts
 import ru.sokomishalov.skraper.provider.ninegag.NinegagSkraper
 import ru.sokomishalov.skraper.provider.ninegag.getTagPosts
+import ru.sokomishalov.skraper.provider.reddit.RedditSkraper
+import ru.sokomishalov.skraper.provider.reddit.getCommunityHotPosts
 import ru.sokomishalov.skraper.provider.twitter.TwitterSkraper
 import ru.sokomishalov.skraper.provider.twitter.getUserPosts
 import kotlin.coroutines.CoroutineContext
@@ -23,26 +27,29 @@ class ListViewActivity : AppCompatActivity(), CoroutineScope {
     override val coroutineContext: CoroutineContext get() = job + Dispatchers.Main
 
     private val job: Job = Job()
-    private val listViewAdapter: ListViewModelAdapter = ListViewModelAdapter(context = this, data = mutableListOf())
+    private val postsAdapter: PostsAdapter = PostsAdapter(context = this, data = mutableListOf())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_view)
         fetchItems()
-        val listView = findViewById<ListView>(R.id.postsListView)
-        listView.adapter = listViewAdapter
+        findViewById<ListView>(R.id.posts_list_view).adapter = postsAdapter
     }
 
     private fun fetchItems() = launch {
         val items = withContext(Dispatchers.Default) {
             listOf(
-                NinegagSkraper(client = DEFAULT_CLIENT).getTagPosts(tag = "meme", limit = DEFAULT_LIMIT),
-                TwitterSkraper(client = DEFAULT_CLIENT).getUserPosts(username = "memes", limit = DEFAULT_LIMIT),
+                RedditSkraper(client = DEFAULT_CLIENT).getCommunityHotPosts(community = "r/videos", limit = DEFAULT_LIMIT),
                 FacebookSkraper(client = DEFAULT_CLIENT).getUserPosts(username = "memes", limit = DEFAULT_LIMIT),
-                InstagramSkraper(client = DEFAULT_CLIENT).getUserPosts(username = "memes", limit = DEFAULT_LIMIT)
-            ).flatten()
+                InstagramSkraper(client = DEFAULT_CLIENT).getUserPosts(username = "memes", limit = DEFAULT_LIMIT),
+                TwitterSkraper(client = DEFAULT_CLIENT).getUserPosts(username = "memes", limit = DEFAULT_LIMIT),
+                NinegagSkraper(client = DEFAULT_CLIENT).getTagPosts(tag = "meme", limit = DEFAULT_LIMIT)
+            ).flatten().sortedByDescending { it.publishedAt }
         }
-        listViewAdapter + items
+
+        findViewById<ProgressBar>(R.id.posts_list_view_progress_bar).visibility = View.GONE
+
+        postsAdapter + items
     }
 
     override fun onDestroy() {
---
+++ example/android/app/src/main/java/ru/sokomishalov/skraper/example/adapter/PostsAdapter.kt
@@ -0,0 +1,72 @@
+package ru.sokomishalov.skraper.example.adapter
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.VideoView
+import com.squareup.picasso.Picasso
+import ru.sokomishalov.skraper.example.R
+import ru.sokomishalov.skraper.model.AttachmentType.IMAGE
+import ru.sokomishalov.skraper.model.AttachmentType.VIDEO
+import ru.sokomishalov.skraper.model.Post
+
+class PostsAdapter(
+    private val context: Context,
+    private val data: MutableList<Post>
+) : BaseAdapter() {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
+        val (view, vh) = when (convertView) {
+            null -> {
+                val view = LayoutInflater.from(context).inflate(R.layout.view_item, parent, false)
+                view to ViewHolder(view)
+            }
+            else -> convertView to convertView.tag as ViewHolder
+        }
+
+        view.tag = vh
+
+        val post = data[position]
+        val attachment = post.attachments.firstOrNull()
+
+        if (attachment != null) {
+            when (attachment.type) {
+                IMAGE -> {
+                    Picasso
+                        .get()
+                        .load(attachment.url)
+                        .into(vh.image)
+                }
+                VIDEO -> {
+                    Log.i("Kek", attachment.url)
+                    vh.video.setVideoPath(attachment.url)
+                    vh.video.start()
+                }
+            }
+        }
+
+        vh.tvTitle.text = data[position].text
+
+        return view
+    }
+
+    override fun getItem(position: Int): Any = data[position]
+    override fun getItemId(position: Int): Long = position.toLong()
+    override fun getCount(): Int = data.size
+
+    operator fun plus(posts: List<Post>) {
+        data += posts
+        notifyDataSetChanged()
+    }
+}
+
+private class ViewHolder(view: View) {
+    val video: VideoView = view.findViewById(R.id.video_attachment)
+    val image: ImageView = view.findViewById(R.id.image_attachment)
+    val tvTitle: TextView = view.findViewById(R.id.tvContent)
+}
