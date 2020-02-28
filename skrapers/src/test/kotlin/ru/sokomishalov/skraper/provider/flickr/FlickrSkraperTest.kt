0ca284c282e3b6e063be6b839218b288fc67b4f1
sokomishalov <sokolovm@dev.vtb>
2020-02-28 14:59:45 +0300

    changed license headers

--- skrapers/src/test/kotlin/ru/sokomishalov/skraper/provider/flickr/FlickrSkraperTest.kt
+++ skrapers/src/test/kotlin/ru/sokomishalov/skraper/provider/flickr/FlickrSkraperTest.kt
@@ -1,5 +1,5 @@
 /**
- * Copyright 2019-2020 the original author or authors.
+ * Copyright (c) 2019-present Mikhael Sokolov
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
