d78ceb67eebe8953eab216b7d4b89cc846eedd60
Mikhael Sokolov <mikhael.sokolov@mynd.co>
2022-08-29 23:19:51 +0300

    spotify implementation

--- skrapers/src/main/kotlin/ru/sokomishalov/skraper/provider/spotify/SpotifySkraper.kt
+++ skrapers/src/main/kotlin/ru/sokomishalov/skraper/provider/spotify/SpotifySkraper.kt
@@ -9,25 +9,32 @@ import ru.sokomishalov.skraper.client.HttpRequest
 import ru.sokomishalov.skraper.client.SkraperClient
 import ru.sokomishalov.skraper.client.fetchDocument
 import ru.sokomishalov.skraper.client.fetchOpenGraphMedia
+import ru.sokomishalov.skraper.internal.jsoup.getMetaPropertyMap
 import ru.sokomishalov.skraper.internal.net.host
+import ru.sokomishalov.skraper.internal.serialization.getString
+import ru.sokomishalov.skraper.internal.serialization.readJsonNodes
 import ru.sokomishalov.skraper.model.*
 
 open class SpotifySkraper @JvmOverloads constructor(
     override val client: SkraperClient = Skrapers.client
 ) : Skraper {
 
+    // https://github.com/AliAkhtari78/SpotifyScraper/blob/6af67de1615a6c931c2be789aefb9e658fe2267f/SpotifyScraper/scraper.py
     override fun getPosts(path: String): Flow<Post> = flow {
         val document = fetchPage(path)
-        println(document)
-
+        val token = document.extractToken() ?: return@flow
+        println(token)
     }
 
     override suspend fun getPageInfo(path: String): PageInfo? {
         val document = fetchPage(path)
-        println(document)
+        val properties = document.getMetaPropertyMap()
 
         return PageInfo(
-
+            nick = properties["og:title"].orEmpty(),
+            name = properties["og:title"].orEmpty(),
+            description = properties["og:description"].orEmpty(),
+            avatar = properties["og:image"]?.toImage(),
         )
     }
 
@@ -43,11 +50,21 @@ open class SpotifySkraper @JvmOverloads constructor(
     }
 
     private suspend fun fetchPage(path: String): Document? {
-        return client.fetchDocument(HttpRequest(BASE_URL.buildFullURL(path)))
+        return client.fetchDocument(HttpRequest(url = BASE_URL.buildFullURL(path)))
     }
+
+    private fun Document?.extractToken(): String?{
+        return this
+            ?.getElementById("session")
+            ?.html()
+            ?.readJsonNodes()
+            ?.getString("accessToken")
+    }
+
 
 
     companion object {
         const val BASE_URL: String = "https://open.spotify.com"
+        const val API_BASE_URL: String = "https://api-partner.spotify.com"
     }
 }
